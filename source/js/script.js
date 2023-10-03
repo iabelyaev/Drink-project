56a5ec5222d6a565cbe4d6c432e782d4adc70514
Yaroslav <yibelyaev@icloud.com>
2023-10-03 19:26:26 +0300

    first commit

---
+++ source/js/script.js
@@ -0,0 +1,7 @@
+const menuBurger = document.querySelector('.menu__burger');
+const list = document.querySelector('.menu__list');
+
+menuBurger.addEventListener('click', () => {
+  list.classList.toggle('menu__list--opened');
+  menuBurger.classList.toggle('menu__burger--opened');
+})
